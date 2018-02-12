fdb6df626b65d0b0bfcd03aabb4b3dba241c28a0
Trent <trenthendricks2@gmail.com>
2018-02-12 09:55:00 -0600

    filled in all options

--- sql-publisher-author-genre/sql-publisher-author-genre.js
+++ sql-publisher-author-genre/sql-publisher-author-genre.js
@@ -9,11 +9,17 @@ var Schemas = {
   OPTION_6: 'option 6'
 };
 
-var sqlPublisherAuthorSchema =  Schemas.FIX_ME; // TODO: Update this constant
+var sqlPublisherAuthorSchema =  Schemas.OPTION_3; // TODO: Update this constant
 // TODO: Write a comment here explaining your choice
+// i chose option_3 because it was setup so that each publisher could have multiple authors since there id was attached to
+// the publishers id
 
-var sqlPublisherGenreSchema =  Schemas.FIX_ME; // TODO: Update this constant
-// TODO: Write a comment here explaining your choice
 
-var sqlAuthorGenreSchema =  Schemas.FIX_ME; // TODO: Update this constant
+var sqlPublisherGenreSchema =  Schemas.OPTION_6; // TODO: Update this constant
 // TODO: Write a comment here explaining your choice
+// i chose options_6 because it had a seperate genre and publisher that were both attached, so this enabled an author to have
+// multiple publishers and genres.
+var sqlAuthorGenreSchema =  Schemas.OPTION_4; // TODO: Update this constant
+// TODO: Write a comment here explaining your choice
+// i went with option_4 because it alowed multiple genres with its name catagory, and each genre had and id
+// that could be stored on the author saying what genre they were writing in
